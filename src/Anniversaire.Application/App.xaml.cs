51a340849fb5914d88f62bfeae0cc94dd5f43cd9
Alexis C. Gridel <contact@agdl.dev>
2022-09-17 09:19:01 +0200

    feat: add maui project application

---
+++ src/Anniversaire.Application/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Anniversaire.Application;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/Anniversaire.Application/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Anniversaire.Application;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
