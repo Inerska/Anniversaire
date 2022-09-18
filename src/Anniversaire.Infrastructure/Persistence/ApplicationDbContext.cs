1f9610fa48743eb8cf3fa01b6bf0702a0ecae9ad
Alexis C. Gridel <contact@agdl.dev>
2022-09-18 21:55:40 +0200

    feat: dbcontext with sqlite provider, first initial migration

--- src/Anniversaire.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Anniversaire.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -2,9 +2,17 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using Anniversaire.Domain.AnniversaireAggregate;
+using Microsoft.EntityFrameworkCore;
+
 namespace Anniversaire.Infrastructure.Persistence;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public DbSet<Birthday> Birthdays { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=anniversaire.db");
+    }
 }
