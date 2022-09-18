cfb0eb4f449695bab86229374d59eeccae6b2069
Alexis C. Gridel <contact@agdl.dev>
2022-09-18 17:21:47 +0200

    feat: add dto and empty db context

---
+++ src/Anniversaire.Domain/AnniversaireAggregate/Birthday.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Anniversaire.Domain.AnniversaireAggregate;
+
+public class Birthday
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+    
+    public DateTime Date { get; set; }
+    
+    public string? Notes { get; set; }
+}
